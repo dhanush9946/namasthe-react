d4ade74a1609f16b98164cb05e19c5b093389b7a
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-07-31 19:07:33 +0530

    Added new files and changes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namasthe React
+
+ #parcel
+ Dev  build
+ local server
+ HMR = hot module replacement
+ file watching algorithm - written in c++
+ caching - faster  builds
+ image optimization
+ minification
+ bundling 
+ compress
+ consistent hashing
+ code splitting
+ differential bundling - support  older browsers
+ diagnostic
+ error handling
+ HTTPs
+ tree shaking - remove unused code
+ different dev and prod bundles
--- app.js
+++ app.js
@@ -1,10 +1,74 @@
-const parent = React.createElement("div",{id:"parent"},
-  [React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"I'm an h1 tag"),React.createElement("h2",{},"I'm an h2 tag")]),
-  
-  React.createElement("div",{id:"child"},
-  [React.createElement("h1",{},"I'm an h1 tag"),React.createElement("h2",{},"I'm an h2 tag")])]);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  root.render(parent);
+// JSX => Babel transpile i
+
+const Jsxheading = ()=> (<h1 className="head" tabIndex="1">Namasthe react using jsx
+
+</h1>);
+
+
+// React component
+// class based component - old
+// functional component - new
+
+
+
+//  component composition
+const HeadingComponent = ()=>(
+  <div> 
+    <Jsxheading />
+    <h1>Namasthe React Functional Component</h1>
+  </div>
+);
+
+
+
+
+
+const value = 10000;
+
+
+
+
+const First = function() {
+  return(
+  <div className="first">
+    <h1>This is the first component</h1>
+    <p>are you enjoy your study</p>
+  </div>
+);
+}
+
+
+const Second = ()=> (
+  <div className="second">
+    <h2>{value}</h2>
+    <First />
+    <h1>This is the second component</h1>
+    <p>it working component composition</p>
+  </div>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Second />);
+
